77e687c10dcd37b9571a58edcb1c56df2535d41d
Akshaya HP <akshayhulkuli@gmail.com>
2024-03-10 18:40:26 +0530

    initialising the data

---
+++ src/main/java/com/akshay/basics/Main.java
@@ -0,0 +1,9 @@
+package com.akshay.basics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world0");
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+    }
+}
