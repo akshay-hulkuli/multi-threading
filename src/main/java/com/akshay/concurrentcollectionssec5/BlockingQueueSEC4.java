8b47b6d92649119c55ca324af1cb3d417f7f4adf
Akshaya HP <akshayhulkuli@gmail.com>
2024-04-07 20:02:57 +0530

    saving changes

--- src/main/java/com/akshay/concurrentcollectionssec5/BlockingQueueSEC4.java
+++ src/main/java/com/akshay/concurrentcollectionssec5/BlockingQueueSEC4.java
@@ -1,4 +1,61 @@
 package com.akshay.concurrentcollectionssec5;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class BlockingQueueSEC4 {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(10);
+        FirstWorker firstWorker = new FirstWorker(blockingQueue);
+        SecondWorker secondWorker = new SecondWorker(blockingQueue);
+        new Thread(firstWorker).start();
+        new Thread(secondWorker).start();
+    }
+}
+
+class FirstWorker implements Runnable {
+
+    BlockingQueue<Integer> blockingQueue;
+
+    public FirstWorker(BlockingQueue<Integer> blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        int counter = 0;
+        while (true) {
+            try {
+                blockingQueue.put(counter);
+                System.out.println("Inserting the count : " + counter);
+                counter++;
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class SecondWorker implements Runnable {
+
+    BlockingQueue<Integer> blockingQueue;
+
+    public SecondWorker(BlockingQueue<Integer> blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                int counter = blockingQueue.take();
+                System.out.println("Removed the count : " + counter);
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
